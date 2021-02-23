8ec11d81f6e7cb8e8a899e843457c9a3ece113a3
ts_ci <ts_ci@github.com>
2021-02-23 08:52:37 +0000

    Enabling shorter import paths [automatic]

---
+++ bin/build-keycloak-theme/generateDebugFiles/index.d.ts
@@ -0,0 +1,5 @@
+/** Files for being able to run a hot reload keycloak container */
+export declare function generateDebugFiles(params: {
+    packageJsonName: string;
+    keycloakThemeBuildingDirPath: string;
+}): void;
