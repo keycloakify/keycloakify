4f8d40a6bb35b870d86003c922f84f338f5c9e9e
ts_ci <ts_ci@github.com>
2021-02-23 12:15:10 +0000

    Enabling shorter import paths [automatic]

---
+++ bin/build-keycloak-theme/generateKeycloakThemeResources.js
@@ -0,0 +1,76 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateKeycloakThemeResources = void 0;
+var transformCodebase_1 = require("../../tools/transformCodebase");
+var fs = __importStar(require("fs"));
+var path_1 = require("path");
+var replaceImportFromStatic_1 = require("./replaceImportFromStatic");
+var generateFtl_1 = require("./generateFtl");
+var ftlValuesGlobalName = "keycloakFtlValues";
+function generateKeycloakThemeResources(params) {
+    var themeName = params.themeName, reactAppBuildDirPath = params.reactAppBuildDirPath, keycloakThemeBuildingDirPath = params.keycloakThemeBuildingDirPath;
+    var themeDirPath = path_1.join(keycloakThemeBuildingDirPath, "src", "main", "resources", "theme", themeName, "login");
+    var allCssGlobalsToDefine = {};
+    transformCodebase_1.transformCodebase({
+        "destDirPath": path_1.join(themeDirPath, "resources"),
+        "srcDirPath": reactAppBuildDirPath,
+        "transformSourceCodeString": function (_a) {
+            var filePath = _a.filePath, sourceCode = _a.sourceCode;
+            if (/\.css?$/i.test(filePath)) {
+                var _b = replaceImportFromStatic_1.replaceImportFromStaticInCssCode({ "cssCode": sourceCode.toString("utf8") }), cssGlobalsToDefine = _b.cssGlobalsToDefine, fixedCssCode = _b.fixedCssCode;
+                allCssGlobalsToDefine = __assign(__assign({}, allCssGlobalsToDefine), cssGlobalsToDefine);
+                return { "modifiedSourceCode": Buffer.from(fixedCssCode, "utf8") };
+            }
+            if (/\.js?$/i.test(filePath)) {
+                var fixedJsCode = replaceImportFromStatic_1.replaceImportFromStaticInJsCode({
+                    "jsCode": sourceCode.toString("utf8"),
+                    ftlValuesGlobalName: ftlValuesGlobalName
+                }).fixedJsCode;
+                return { "modifiedSourceCode": Buffer.from(fixedJsCode, "utf8") };
+            }
+            return { "modifiedSourceCode": sourceCode };
+        }
+    });
+    var generateFtlFilesCode = generateFtl_1.generateFtlFilesCodeFactory({
+        "cssGlobalsToDefine": allCssGlobalsToDefine,
+        ftlValuesGlobalName: ftlValuesGlobalName,
+        "indexHtmlCode": fs.readFileSync(path_1.join(reactAppBuildDirPath, "index.html")).toString("utf8")
+    }).generateFtlFilesCode;
+    ["login.ftl", "register.ftl"].forEach(function (pageBasename) {
+        var ftlCode = generateFtlFilesCode({ pageBasename: pageBasename }).ftlCode;
+        fs.writeFileSync(path_1.join(themeDirPath, pageBasename), Buffer.from(ftlCode, "utf8"));
+    });
+    fs.writeFileSync(path_1.join(themeDirPath, "theme.properties"), Buffer.from("parent=base\n", "utf8"));
+}
+exports.generateKeycloakThemeResources = generateKeycloakThemeResources;
+//# sourceMappingURL=generateKeycloakThemeResources.js.map
