071635ea0593ccb201f386bdc8802990145ce15a
ts_ci <ts_ci@github.com>
2021-03-02 23:06:50 +0000

    Enabling shorter import paths [automatic]

---
+++ lib/i18n/useKcLanguageTag.d.ts
@@ -0,0 +1,1 @@
+export declare const useKcLanguageTag: () => import("powerhooks").UseNamedStateReturnType<"tr" | "no" | "en" | "ca" | "cs" | "de" | "es" | "fr" | "it" | "ja" | "lt" | "nl" | "pl" | "pt_BR" | "ru" | "sk" | "sv" | "zh_CN", "kcLanguageTag">;
