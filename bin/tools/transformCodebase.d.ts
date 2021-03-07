202d003cc5cf7cfd15e5b42e860a866a1399e727
ts_ci <ts_ci@github.com>
2021-03-07 14:47:01 +0000

    Enabling shorter import paths [automatic]

---
+++ bin/tools/transformCodebase.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="node" />
+declare type TransformSourceCode = (params: {
+    sourceCode: Buffer;
+    filePath: string;
+}) => {
+    modifiedSourceCode: Buffer;
+    newFileName?: string;
+} | undefined;
+/** Apply a transformation function to every file of directory */
+export declare function transformCodebase(params: {
+    srcDirPath: string;
+    destDirPath: string;
+    transformSourceCode?: TransformSourceCode;
+}): void;
+export {};
---
+++ lib/i18n/useKcLanguageTag.d.ts
@@ -0,0 +1,1 @@
+export declare const useKcLanguageTag: () => import("powerhooks").UseNamedStateReturnType<"tr" | "no" | "en" | "ca" | "cs" | "de" | "es" | "fr" | "it" | "ja" | "lt" | "nl" | "pl" | "pt-BR" | "ru" | "sk" | "sv" | "zh-CN", "kcLanguageTag">;
