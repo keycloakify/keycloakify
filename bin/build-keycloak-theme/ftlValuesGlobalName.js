7ee43b0c52e3d8d8bd83c1ba74f6df4fec7ac818
ts_ci <ts_ci@github.com>
2021-03-10 20:34:42 +0000

    Enabling shorter import paths [automatic]

---
+++ bin/build-keycloak-theme/ftlValuesGlobalName.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ftlValuesGlobalName = void 0;
+exports.ftlValuesGlobalName = "kcContext";
+//# sourceMappingURL=ftlValuesGlobalName.js.map
