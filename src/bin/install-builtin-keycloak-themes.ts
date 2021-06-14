cad1f8b957ce8fd3620bc558434be2a0e6a846b1
garronej <joseph.garrone.gj@gmail.com>
2021-06-14 19:05:50 +0200

    Fix default background bug

--- src/bin/install-builtin-keycloak-themes.ts
+++ src/bin/install-builtin-keycloak-themes.ts
@@ -5,7 +5,7 @@ import { downloadAndUnzip } from "./tools/downloadAndUnzip";
 import { join as pathJoin } from "path";
 
 export const builtinThemesUrl =
-    "https://github.com/garronej/keycloakify/releases/download/v0.0.1/keycloak_11.0.3_builtin_themes_with_light_mods.zip";
+    "https://github.com/garronej/keycloakify/releases/download/v0.0.1/keycloak_11.0.3_builtin_themes.zip";
 
 if (require.main === module) {
 
