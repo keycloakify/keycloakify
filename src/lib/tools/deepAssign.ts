3ecb63d5004b3cbcb2184a80e869bd9879a63e72
garronej <joseph.garrone.gj@gmail.com>
2021-11-08 19:33:06 +0100

    Fix deepClone so we can overwrite with undefined in when we mock kcContext

--- src/lib/tools/deepAssign.ts
+++ src/lib/tools/deepAssign.ts
@@ -1,32 +1,6 @@
 import { assert } from "tsafe/assert";
 import { is } from "tsafe/is";
-
-function deepClone<T>(src: T): T {
-    const generateId = (() => {
-        const prefix = "xIfKdLsIIdJdLdOeJqePe";
-
-        let counter = 0;
-
-        return () => `${prefix}${counter++}`;
-    })();
-
-    const map = new Map<string, Function>();
-
-    return JSON.parse(
-        JSON.stringify(src, (...[, value]) => {
-            if (typeof value === "function") {
-                const id = generateId();
-
-                map.set(id, value);
-
-                return id;
-            }
-
-            return value;
-        }),
-        (...[, value]) => (typeof value === "string" && map.has(value) ? map.get(value) : value),
-    );
-}
+import { deepClone } from "./deepClone";
 
 //Warning: Be mindful that because of array this is not idempotent.
 export function deepAssign(params: { target: Record<string, unknown>; source: Record<string, unknown> }) {
--- src/lib/tools/deepClone.ts
+++ src/lib/tools/deepClone.ts
@@ -1,3 +1,17 @@
-export function deepClone<T>(arg: T): T {
-    return JSON.parse(JSON.stringify(arg));
+import "minimal-polyfills/Object.fromEntries";
+
+export function deepClone<T>(o: T): T {
+    if (!(o instanceof Object)) {
+        return o;
+    }
+
+    if (typeof o === "function") {
+        return o;
+    }
+
+    if (o instanceof Array) {
+        return o.map(deepClone) as any;
+    }
+
+    return Object.fromEntries(Object.entries(o).map(([key, value]) => [key, deepClone(value)])) as any;
 }
