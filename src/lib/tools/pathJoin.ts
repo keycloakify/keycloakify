cb8b165c8e35859e97348e0d35e3578452bf073e
garronej <joseph.garrone.gj@gmail.com>
2021-12-16 23:13:18 +0100

    Fix path.join polyfill

--- src/lib/tools/pathJoin.ts
+++ src/lib/tools/pathJoin.ts
@@ -1,3 +1,6 @@
 export function pathJoin(...path: string[]): string {
-    return path.map(part => part.replace(/^\/+/, "").replace(/\/+$/, "")).join("/");
+    return path
+        .map((part, i) => (i === 0 ? part : part.replace(/^\/+/, "")))
+        .map((part, i) => (i === path.length - 1 ? part : part.replace(/\/+$/, "")))
+        .join("/");
 }
