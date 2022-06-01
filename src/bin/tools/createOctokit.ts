9bdb224631f0262fde2821869128bc76279b3eb6
garronej <joseph.garrone.gj@gmail.com>
2022-06-01 05:51:33 +0200

    Prevent rate limite in CI by authenticating

--- src/bin/tools/createOctokit.ts
+++
@@ -1,7 +0,0 @@
-import { Octokit } from "@octokit/rest";
-
-export function createOctokit(params: { github_token: string }) {
-    const { github_token } = params;
-
-    return new Octokit({ ...(github_token !== "" ? { "auth": github_token } : {}) });
-}
