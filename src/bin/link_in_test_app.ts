58c8306cf467f5884757683cf34428deba55ce57
garronej <joseph.garrone.gj@gmail.com>
2022-06-17 16:32:20 +0200

    Enable users to link keycloak in their own app

--- src/bin/link_in_test_app.ts
+++ src/bin/link_in_test_app.ts
@@ -60,7 +60,7 @@ const execYarnLink = (params: { targetModuleName?: string; cwd: string }) => {
     });
 };
 
-const testAppNames = ["keycloakify-demo-app"] as const;
+const testAppNames = [process.argv[2] ?? "keycloakify-demo-app"] as const;
 
 const getTestAppPath = (testAppName: typeof testAppNames[number]) => pathJoin(keycloakifyDirPath, "..", testAppName);
 
