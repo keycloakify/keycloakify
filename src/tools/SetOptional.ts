02bbedcfca88bd2bf328718aa0b5875ed4400e45
garronej <joseph.garrone@data.gouv.fr>
2023-03-19 14:56:30 +0100

    Remove no longer used tools

--- src/tools/SetOptional.ts
+++
@@ -1,1 +0,0 @@
-export type SetOptional<T extends Record<string, unknown>, K extends keyof T> = Omit<T, K> & Partial<Record<K, T[K]>>;
--- src/tools/allPropertiesValuesToUndefined.ts
+++
@@ -1,5 +0,0 @@
-import "minimal-polyfills/Object.fromEntries";
-
-export function allPropertiesValuesToUndefined<T extends Record<string, unknown>>(obj: T): Record<keyof T, undefined> {
-    return Object.fromEntries(Object.entries(obj).map(([key]) => [key, undefined])) as any;
-}
