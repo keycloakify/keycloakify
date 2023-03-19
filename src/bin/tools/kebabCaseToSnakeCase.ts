b573bc20b504869a520438691074f7bd7b7c2784
garronej <joseph.garrone@data.gouv.fr>
2023-03-19 16:37:35 +0100

    Fix

--- src/bin/tools/kebabCaseToSnakeCase.ts
+++ src/bin/tools/kebabCaseToSnakeCase.ts
@@ -3,5 +3,5 @@ import { capitalize } from "tsafe/capitalize";
 export function kebabCaseToCamelCase(kebabCaseString: string): string {
     const [first, ...rest] = kebabCaseString.split("-");
 
-    return [first, rest.map(capitalize)].join("");
+    return [first, ...rest.map(capitalize)].join("");
 }
