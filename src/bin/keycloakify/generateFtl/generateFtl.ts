32f66b3eaa0dd49420328f9d4237e2aa353d08ea
garronej <joseph.garrone@data.gouv.fr>
2023-04-03 20:16:38 +0200

    #297

--- src/bin/keycloakify/generateFtl/generateFtl.ts
+++ src/bin/keycloakify/generateFtl/generateFtl.ts
@@ -13,37 +13,6 @@ export const themeTypes = ["login", "account"] as const;
 
 export type ThemeType = (typeof themeTypes)[number];
 
-export const loginThemePageIds = [
-    "login.ftl",
-    "login-username.ftl",
-    "login-password.ftl",
-    "webauthn-authenticate.ftl",
-    "register.ftl",
-    "register-user-profile.ftl",
-    "info.ftl",
-    "error.ftl",
-    "login-reset-password.ftl",
-    "login-verify-email.ftl",
-    "terms.ftl",
-    "login-otp.ftl",
-    "login-update-profile.ftl",
-    "login-update-password.ftl",
-    "login-idp-link-confirm.ftl",
-    "login-idp-link-email.ftl",
-    "login-page-expired.ftl",
-    "login-config-totp.ftl",
-    "logout-confirm.ftl",
-    "update-user-profile.ftl",
-    "idp-review-user-profile.ftl",
-    "update-email.ftl",
-    "select-authenticator.ftl"
-] as const;
-
-export const accountThemePageIds = ["password.ftl", "account.ftl"] as const;
-
-export type LoginThemePageId = (typeof loginThemePageIds)[number];
-export type AccountThemePageId = (typeof accountThemePageIds)[number];
-
 export type BuildOptionsLike = BuildOptionsLike.Standalone | BuildOptionsLike.ExternalAssets;
 
 export namespace BuildOptionsLike {
