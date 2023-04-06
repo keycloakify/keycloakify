2d055217897fe8b41ee2da2e655c7204beb7fbb4
Waldemar Reusch <waldemar.reusch@googlemail.com>
2023-04-06 21:34:20 +0200

    fix(jar): fix empty jar

--- src/bin/tools/walk.ts
+++ src/bin/tools/walk.ts
@@ -1,9 +1,9 @@
 import { readdir } from "fs/promises";
-import { resolve } from "path";
+import { resolve, sep } from "path";
 
 /**
  * Asynchronously and recursively walk a directory tree, yielding every file and directory
- * found
+ * found. Directory paths will _always_ end with a path separator.
  *
  * @param root the starting directory
  * @returns AsyncGenerator
@@ -12,8 +12,8 @@ export default async function* walk(root: string): AsyncGenerator<string, void, 
     for (const entry of await readdir(root, { withFileTypes: true })) {
         const absolutePath = resolve(root, entry.name);
         if (entry.isDirectory()) {
-            yield absolutePath;
+            yield absolutePath.endsWith(sep) ? absolutePath : (absolutePath + sep);
             yield* walk(absolutePath);
-        } else yield absolutePath;
+        } else yield absolutePath.endsWith(sep) ? absolutePath.substring(0, absolutePath.length-1) : absolutePath;
     }
 }
