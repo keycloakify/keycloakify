86702d8a73fbe9b4589d1f38a6e10733237820c3
garronej <joseph.garrone@data.gouv.fr>
2023-04-17 01:41:30 +0200

    Avoid deprecating getKcContext

--- src/login/kcContext/getKcContext.ts
+++ src/login/kcContext/getKcContext.ts
@@ -2,7 +2,9 @@ import type { DeepPartial } from "keycloakify/tools/DeepPartial";
 import type { ExtendKcContext } from "./getKcContextFromWindow";
 import { createGetKcContext } from "./createGetKcContext";
 
-/** @deprecated: Use createGetKcContext instead */
+/** NOTE: We now recommend using createGetKcContext instead of this function to make storybook integration easier
+ *  See: https://github.com/keycloakify/keycloakify-starter/blob/main/src/keycloak-theme/account/kcContext.ts
+ */
 export function getKcContext<KcContextExtension extends { pageId: string } = never>(params?: {
     mockPageId?: ExtendKcContext<KcContextExtension>["pageId"];
     mockData?: readonly DeepPartial<ExtendKcContext<KcContextExtension>>[];
