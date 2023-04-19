08831fc31d79c0f9f0182d2dca66e714dfc82d53
garronej <joseph.garrone@data.gouv.fr>
2023-04-20 01:53:36 +0200

    Add storybook Error page #274

---
+++ stories/login/Error.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import type { ComponentMeta } from "@storybook/react";
+import { createPageStory } from "./createPageStory";
+
+const pageId = "error.ftl";
+
+const { PageStory } = createPageStory({ pageId });
+
+const meta: ComponentMeta<any> = {
+    "title": `login/${pageId}`,
+    "component": PageStory,
+    "parameters": {
+        "viewMode": "story",
+        "previewTabs": {
+            "storybook/docs/panel": {
+                "hidden": true
+            }
+        }
+    }
+};
+
+export default meta;
+
+export const Default = () => <PageStory />;
+
+export const WithAnotherMessage = () => <PageStory kcContext={{ "message": { "summary": "With another error message" } }} />;
