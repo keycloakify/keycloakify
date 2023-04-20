f057114bcceedac3edcea338228c542c36a87af1
garronej <joseph.garrone@data.gouv.fr>
2023-04-20 02:54:24 +0200

    Add account/password.ftl to storybook

---
+++ stories/account/pages/Pasword.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import type { ComponentMeta } from "@storybook/react";
+import { createPageStory } from "../createPageStory";
+
+const pageId = "password.ftl";
+
+const { PageStory } = createPageStory({ pageId });
+
+const meta: ComponentMeta<any> = {
+    title: `account/${pageId}`,
+    component: PageStory,
+    parameters: {
+        viewMode: "story",
+        previewTabs: {
+            "storybook/docs/panel": {
+                hidden: true
+            }
+        }
+    }
+};
+
+export default meta;
+
+export const Default = () => <PageStory />;
