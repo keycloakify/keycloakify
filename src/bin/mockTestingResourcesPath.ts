18039140db1a9bf68d04c0f8cf9d0702bd9f7f20
garronej <joseph.garrone@data.gouv.fr>
2023-04-20 04:17:12 +0200

    Important fix, assets common where broken

--- src/bin/mockTestingResourcesPath.ts
+++ src/bin/mockTestingResourcesPath.ts
@@ -2,4 +2,4 @@ import { pathJoin } from "./tools/pathJoin";
 
 export const basenameOfKeycloakDirInPublicDir = "keycloak-resources";
 export const resourcesDirPathRelativeToPublicDir = pathJoin(basenameOfKeycloakDirInPublicDir, "resources");
-export const resourcesCommonDirPathRelativeToPublicDir = pathJoin(basenameOfKeycloakDirInPublicDir, "resources_common");
+export const resourcesCommonDirPathRelativeToPublicDir = pathJoin(resourcesDirPathRelativeToPublicDir, "resources_common");
