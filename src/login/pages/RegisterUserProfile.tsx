e8b8be48995fbc0b345d454282cc704615ebb7f9
D-Simona-G <doina.graur@stfc.ac.uk>
2023-12-15 12:16:20 +0000

    Adds recaptcha script management

--- src/login/pages/RegisterUserProfile.tsx
+++ src/login/pages/RegisterUserProfile.tsx
@@ -6,6 +6,25 @@ import { useGetClassName } from "keycloakify/login/lib/useGetClassName";
 import type { KcContext } from "../kcContext";
 import type { I18n } from "../i18n";
 
+declare global {
+    interface Window {
+        grecaptcha: {
+            render: (
+                container?: string | HTMLElement,
+                options?: {
+                    sitekey: string;
+                    theme?: string;
+                    size?: string;
+                    tabindex?: number;
+                    callback?: (response: string) => void;
+                    "expired-callback"?: () => void;
+                    "error-callback"?: () => void;
+                }
+            ) => number;
+        };
+    }
+}
+
 export default function RegisterUserProfile(props: PageProps<Extract<KcContext, { pageId: "register-user-profile.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
 
@@ -21,6 +40,42 @@ export default function RegisterUserProfile(props: PageProps<Extract<KcContext, 
     const { msg, msgStr } = i18n;
 
     const [isFormSubmittable, setIsFormSubmittable] = useState(false);
+
+    useEffect(() => {
+        if (recaptchaRequired && recaptchaSiteKey) {
+            const recaptchaScriptUrl = kcContext.scripts?.[0] || "https://www.google.com/recaptcha/api.js"; // fallback for Storybook
+
+            let script = document.querySelector(`script[src="${recaptchaScriptUrl}"]`) as HTMLScriptElement | null;
+
+            const renderRecaptcha = () => {
+                if (window.grecaptcha && document.getElementById("recaptcha-container")) {
+                    window.grecaptcha.render("recaptcha-container", {
+                        sitekey: recaptchaSiteKey
+                        // other reCAPTCHA options as needed
+                    });
+                }
+            };
+
+            if (!script) {
+                script = document.createElement("script");
+                script.src = recaptchaScriptUrl;
+                script.async = true;
+                document.head.appendChild(script);
+
+                script.onload = () => {
+                    setTimeout(renderRecaptcha, 500);
+                };
+            } else {
+                setTimeout(renderRecaptcha, 500);
+            }
+
+            return () => {
+                if (document.body.contains(script)) {
+                    document.body.removeChild(script!);
+                }
+            };
+        }
+    }, [recaptchaRequired, recaptchaSiteKey, kcContext.scripts]);
 
     return (
         <Template
@@ -39,7 +94,7 @@ export default function RegisterUserProfile(props: PageProps<Extract<KcContext, 
                 {recaptchaRequired && (
                     <div className="form-group">
                         <div className={getClassName("kcInputWrapperClass")}>
-                            <div className="g-recaptcha" data-size="compact" data-sitekey={recaptchaSiteKey} />
+                            <div className="g-recaptcha" id="recaptcha-container" data-size="compact" data-sitekey={recaptchaSiteKey} />
                         </div>
                     </div>
                 )}
@@ -69,4 +124,7 @@ export default function RegisterUserProfile(props: PageProps<Extract<KcContext, 
             </form>
         </Template>
     );
+}
+function useEffect(arg0: () => (() => void) | undefined, arg1: (string | boolean | string[] | undefined)[]) {
+    throw new Error("Function not implemented.");
 }
