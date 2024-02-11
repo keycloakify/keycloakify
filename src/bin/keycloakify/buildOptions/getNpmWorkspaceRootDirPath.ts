43dcce84784689765c644f8c8f47431ed3337137
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-02-11 20:20:38 +0100

    Add cache to getNpmWorkspaceRootDirPath

--- src/bin/keycloakify/buildOptions/getNpmWorkspaceRootDirPath.ts
+++ src/bin/keycloakify/buildOptions/getNpmWorkspaceRootDirPath.ts
@@ -2,8 +2,25 @@ import * as child_process from "child_process";
 import { join as pathJoin, resolve as pathResolve, sep as pathSep } from "path";
 import { assert } from "tsafe/assert";
 
+let cache:
+    | {
+          reactAppRootDirPath: string;
+          npmWorkspaceRootDirPath: string;
+      }
+    | undefined = undefined;
+
 export function getNpmWorkspaceRootDirPath(params: { reactAppRootDirPath: string }) {
     const { reactAppRootDirPath } = params;
+
+    use_cache: {
+        if (cache === undefined || cache.reactAppRootDirPath !== reactAppRootDirPath) {
+            break use_cache;
+        }
+
+        const { npmWorkspaceRootDirPath } = cache;
+
+        return { npmWorkspaceRootDirPath };
+    }
 
     const npmWorkspaceRootDirPath = (function callee(depth: number): string {
         const cwd = pathResolve(pathJoin(...[reactAppRootDirPath, ...Array(depth).fill("..")]));
@@ -22,6 +39,11 @@ export function getNpmWorkspaceRootDirPath(params: { reactAppRootDirPath: string
 
         return cwd;
     })(0);
+
+    cache = {
+        reactAppRootDirPath,
+        npmWorkspaceRootDirPath
+    };
 
     return { npmWorkspaceRootDirPath };
 }
