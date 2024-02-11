b3c242595ec2a936a2ea2eb10e4ab411a6ca8971
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-02-11 10:36:50 +0100

    Fix PUBLIC_URL typing

--- src/PUBLIC_URL.ts
+++ src/PUBLIC_URL.ts
@@ -1,4 +1,5 @@
 import { nameOfTheGlobal, basenameOfTheKeycloakifyResourcesDir } from "keycloakify/bin/constants";
+import { assert } from "tsafe/assert";
 
 /**
  * This is an equivalent of process.env.PUBLIC_URL thay you can use in Webpack projects.
@@ -7,7 +8,14 @@ import { nameOfTheGlobal, basenameOfTheKeycloakifyResourcesDir } from "keycloaki
 export const PUBLIC_URL = (() => {
     const kcContext = (window as any)[nameOfTheGlobal];
 
-    return kcContext === undefined || process.env.NODE_ENV === "development"
-        ? process.env.PUBLIC_URL
-        : `${kcContext.url.resourcesPath}/${basenameOfTheKeycloakifyResourcesDir}`;
+    if (kcContext === undefined || process.env.NODE_ENV === "development") {
+        assert(
+            process.env.PUBLIC_URL !== undefined,
+            `If you use keycloakify/PUBLIC_URL you should be in Webpack and thus process.env.PUBLIC_URL should be defined`
+        );
+
+        return process.env.PUBLIC_URL;
+    }
+
+    return `${kcContext.url.resourcesPath}/${basenameOfTheKeycloakifyResourcesDir}`;
 })();
