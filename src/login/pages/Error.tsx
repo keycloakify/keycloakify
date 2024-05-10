8d0d17910c2519a1a5168a3dcc5462f4868d00c4
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-05-10 21:51:46 +0200

    Update error.ftl

--- src/login/pages/Error.tsx
+++ src/login/pages/Error.tsx
@@ -5,7 +5,7 @@ import type { I18n } from "../i18n";
 export default function Error(props: PageProps<Extract<KcContext, { pageId: "error.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
 
-    const { message, client } = kcContext;
+    const { message, client, skipLink } = kcContext;
 
     const { msg } = i18n;
 
@@ -13,7 +13,7 @@ export default function Error(props: PageProps<Extract<KcContext, { pageId: "err
         <Template {...{ kcContext, i18n, doUseDefaultCss, classes }} displayMessage={false} headerNode={msg("errorTitle")}>
             <div id="kc-error-message">
                 <p className="instruction">{message.summary}</p>
-                {client !== undefined && client.baseUrl !== undefined && (
+                {!skipLink && client !== undefined && client.baseUrl !== undefined && (
                     <p>
                         <a id="backToApplication" href={client.baseUrl}>
                             {msg("backToApplication")}
