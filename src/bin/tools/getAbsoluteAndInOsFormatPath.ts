d85ab889b39ad522e9ed5369b054972704589d16
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-05-18 09:15:39 +0200

    Support "~" in getAbsoluteAndInOsFormat

--- src/bin/tools/getAbsoluteAndInOsFormatPath.ts
+++ src/bin/tools/getAbsoluteAndInOsFormatPath.ts
@@ -1,4 +1,5 @@
-import { isAbsolute as pathIsAbsolute, sep as pathSep, join as pathJoin } from "path";
+import { isAbsolute as pathIsAbsolute, sep as pathSep, join as pathJoin, resolve as pathResolve } from "path";
+import * as os from "os";
 
 export function getAbsoluteAndInOsFormatPath(params: { pathIsh: string; cwd: string }): string {
     const { pathIsh, cwd } = params;
@@ -7,11 +8,17 @@ export function getAbsoluteAndInOsFormatPath(params: { pathIsh: string; cwd: str
 
     pathOut = pathOut.replace(/\//g, pathSep);
 
+    if (pathOut.startsWith("~")) {
+        pathOut = pathOut.replace("~", os.homedir());
+    }
+
     pathOut = pathOut.endsWith(pathSep) ? pathOut.slice(0, -1) : pathOut;
 
     if (!pathIsAbsolute(pathOut)) {
         pathOut = pathJoin(cwd, pathOut);
     }
+
+    pathOut = pathResolve(pathOut);
 
     return pathOut;
 }
