157e4ac485832248bd709ab4e0916a4077b8fa95
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-03 01:23:28 +0200

    Add missing storybook pages

---
+++ stories/login/pages/DeleteCredential.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { createPageStory, parameters } from "../createPageStory";
+
+const pageId = "delete-credential.ftl";
+
+const { PageStory } = createPageStory({ pageId });
+
+const meta = {
+    title: `login/${pageId}`,
+    component: PageStory,
+    parameters
+} satisfies Meta<typeof PageStory>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: () => <PageStory />
+};
