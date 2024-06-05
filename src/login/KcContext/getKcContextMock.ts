aa8dc1919f13a37e7fdb0242a3f641877797fe79
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-05 23:22:00 +0200

    Make getKcContext mock return type less narrow

--- src/login/KcContext/getKcContextMock.ts
+++ src/login/KcContext/getKcContextMock.ts
@@ -47,7 +47,9 @@ export function createGetKcContextMock<
     >(params: {
         pageId: PageId;
         overrides?: DeepPartial<Extract<KcContext, { pageId: PageId }>>;
-    }): Extract<KcContext, { pageId: PageId }> {
+        // NOTE: We choose to have a return type less precise than Extract<KcContext, { pageId: PageId }> {
+        // because we want to be able to use the mock just as the real KcContext.
+    }): KcContext {
         const { pageId, overrides } = params;
 
         const kcContextMock = structuredCloneButFunctions(
