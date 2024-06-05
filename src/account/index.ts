6bcdf286ef7d5cf5bd9cb60751477541e74089dc
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-05 22:53:14 +0200

    Add missing export

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,3 +1,4 @@
 export type { ExtendKcContext } from "keycloakify/account/KcContext";
+export type { PageProps } from "keycloakify/account/pages/PageProps";
 export { createGetKcContextMock } from "keycloakify/account/KcContext";
 export { createUseI18n } from "keycloakify/account/i18n";
