c52157bfb9f1e9a07d42355e3c1aa00946d63444
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-06 04:35:57 +0200

    Remove dependency to powerhooks

--- src/tools/useOnFirstMount.ts
+++ src/tools/useOnFirstMount.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useConst } from "powerhooks/useConst";
+import { useConst } from "./useConst";
 import { id } from "tsafe/id";
 
 /** Callback is guaranteed to be call only once per component mount event in strict mode */
