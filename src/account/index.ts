a7f3e008210c8d2ec71e5b935812d3c68f4c712a
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-06 06:27:28 +0200

    Remove createKcContextMock from the index

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,4 +1,3 @@
 export type { ExtendKcContext } from "keycloakify/account/KcContext";
 export type { PageProps } from "keycloakify/account/pages/PageProps";
-export { createGetKcContextMock } from "keycloakify/account/KcContext";
 export { createUseI18n } from "keycloakify/account/i18n";
--- src/login/index.ts
+++ src/login/index.ts
@@ -1,5 +1,4 @@
 export type { ExtendKcContext, Attribute } from "keycloakify/login/KcContext";
 export type { PageProps } from "keycloakify/login/pages/PageProps";
-export { createGetKcContextMock } from "keycloakify/login/KcContext";
 export { useDownloadTerms } from "keycloakify/login/lib/useDownloadTerms";
 export { createUseI18n } from "keycloakify/login/i18n";
