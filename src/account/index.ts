7f917311d8eb10b8611c93a3292e64c4101b5ac5
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-08 07:17:11 +0200

    Export ClassKey in the index instead of PageProps

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,3 +1,3 @@
 export type { ExtendKcContext } from "keycloakify/account/KcContext";
-export type { PageProps } from "keycloakify/account/pages/PageProps";
+export type { ClassKey } from "keycloakify/account/TemplateProps";
 export { createUseI18n } from "keycloakify/account/i18n";
