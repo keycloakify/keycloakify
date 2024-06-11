b7a543f8cb89492e912f38d655dbd850b139e431
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-11 20:30:18 +0200

    Do not export PageProps in the index

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,4 +1,3 @@
 export type { ExtendKcContext } from "keycloakify/account/KcContext";
 export type { ClassKey } from "keycloakify/account/TemplateProps";
-export type { PageProps } from "keycloakify/account/pages/PageProps";
 export { createUseI18n } from "keycloakify/account/i18n";
