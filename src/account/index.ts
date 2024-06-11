c750bf4ee84a019cd81aabb9f039f604ec1acc25
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-11 19:14:04 +0200

    Export PageProps

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,3 +1,4 @@
 export type { ExtendKcContext } from "keycloakify/account/KcContext";
 export type { ClassKey } from "keycloakify/account/TemplateProps";
+export type { PageProps } from "keycloakify/account/pages/PageProps";
 export { createUseI18n } from "keycloakify/account/i18n";
--- src/login/index.ts
+++ src/login/index.ts
@@ -1,4 +1,5 @@
 export type { ExtendKcContext, Attribute } from "keycloakify/login/KcContext";
 export type { ClassKey } from "keycloakify/login/TemplateProps";
+export type { PageProps } from "keycloakify/login/pages/PageProps";
 export { useDownloadTerms } from "keycloakify/login/lib/useDownloadTerms";
 export { createUseI18n } from "keycloakify/login/i18n";
