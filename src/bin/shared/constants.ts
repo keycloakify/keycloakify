28c5e2bab2c5f9c33ea6fc082222294c814b0967
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-16 15:19:27 +0200

    Rename use 'dist' instead of 'build' for basenameOfTheKeycloakifyResourcesDir

--- src/bin/shared/constants.ts
+++ src/bin/shared/constants.ts
@@ -3,7 +3,7 @@ export const nameOfTheLocalizationRealmOverridesUserProfileProperty =
 export const keycloak_resources = "keycloak-resources";
 export const resources_common = "resources-common";
 export const lastKeycloakVersionWithAccountV1 = "21.1.2";
-export const basenameOfTheKeycloakifyResourcesDir = "build";
+export const basenameOfTheKeycloakifyResourcesDir = "dist";
 
 export const themeTypes = ["login", "account"] as const;
 export const accountV1ThemeName = "account-v1";
