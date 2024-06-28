249877b9c5e1bb3f32916262f6c6587d2168a52f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-28 19:01:00 +0200

    Better wording for assertNoPnpmDlx

--- src/bin/tools/assertNoPnpmDlx.ts
+++ src/bin/tools/assertNoPnpmDlx.ts
@@ -4,9 +4,9 @@ export function assertNoPnpmDlx() {
     if (__dirname.includes(`${pathSep}pnpm${pathSep}dlx${pathSep}`)) {
         console.log(
             [
-                "Please don't use `pnpm dlx keycloakify`. Even if you're using pnpm as a package manager",
-                "use `npx keycloakify` or `pnpm exec keycloakify` instead since you want to use the keycloakify version that is installed in your project and",
-                "not the latest version on NPM."
+                "Please don't use `pnpm dlx keycloakify`.",
+                "\nUse `npx keycloakify` or `pnpm exec keycloakify` instead since you want to use the",
+                "keycloakify version that is installed in your project and not the latest version on NPM."
             ].join(" ")
         );
         process.exit(1);
