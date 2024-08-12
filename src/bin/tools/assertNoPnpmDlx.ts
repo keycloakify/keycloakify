53e94d04f6ee980c96be769794cdcbbc33be5af4
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-13 00:17:02 +0200

    Improve message related to pnpm dlx

--- src/bin/tools/assertNoPnpmDlx.ts
+++ src/bin/tools/assertNoPnpmDlx.ts
@@ -5,9 +5,11 @@ export function assertNoPnpmDlx() {
     if (__dirname.includes(`${pathSep}pnpm${pathSep}dlx${pathSep}`)) {
         console.log(
             [
-                chalk.red("Please don't use `pnpm dlx keycloakify`"),
+                chalk.red(
+                    "Please don't use `pnpm dlx keycloakify` (download and execute)"
+                ),
                 "\nUse `npx keycloakify` or `pnpm exec keycloakify` instead since you want to use the keycloakify",
-                "version that is installed in your project and not the latest version on NPM."
+                "version that is installed in your project and not download and run the latest NPM version of keycloakify."
             ].join(" ")
         );
         process.exit(1);
