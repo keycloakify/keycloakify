25d31463f46190284c8d5b280f0c8eb411554e2d
garronej <joseph.garrone.gj@gmail.com>
2024-10-25 00:01:12 +0000

    Make script delegation work on windows

--- src/bin/shared/customHandler_delegate.ts
+++ src/bin/shared/customHandler_delegate.ts
@@ -8,7 +8,7 @@ import {
     ApiVersion
 } from "./customHandler";
 import * as child_process from "child_process";
-import { dirname as pathDirname } from "path";
+import { sep as pathSep } from "path";
 import * as fs from "fs";
 
 assert<Equals<ApiVersion, "v1">>();
@@ -19,7 +19,35 @@ export function maybeDelegateCommandToCustomHandler(params: {
 }): { hasBeenHandled: boolean } {
     const { commandName, buildContext } = params;
 
-    if (!fs.readdirSync(pathDirname(process.argv[1])).includes(BIN_NAME)) {
+    const nodeModulesBinDirPath = (() => {
+        const binPath = process.argv[1];
+
+        const segments: string[] = [".bin"];
+
+        let foundNodeModules = false;
+
+        for (const segment of binPath.split(pathSep).reverse()) {
+            skip_segment: {
+                if (foundNodeModules) {
+                    break skip_segment;
+                }
+
+                if (segment === "node_modules") {
+                    foundNodeModules = true;
+                    break skip_segment;
+                }
+
+                continue;
+            }
+
+            segments.unshift(segment);
+        }
+
+        return segments.join(pathSep);
+    })();
+
+    if (!fs.readdirSync(nodeModulesBinDirPath).includes(BIN_NAME)) {
+        console.log(`Custom handler not found`);
         return { hasBeenHandled: false };
     }
 
