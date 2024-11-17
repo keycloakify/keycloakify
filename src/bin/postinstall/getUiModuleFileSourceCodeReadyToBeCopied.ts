397f8133bf736d330a0e0bd664866db10288780c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-11-17 23:08:52 +0100

    Fix comment formatting

--- src/bin/postinstall/getUiModuleFileSourceCodeReadyToBeCopied.ts
+++ src/bin/postinstall/getUiModuleFileSourceCodeReadyToBeCopied.ts
@@ -36,19 +36,21 @@ export async function getUiModuleFileSourceCodeReadyToBeCopied(params: {
         if (isForEjection) {
             return [
                 `/*`,
-                `This file was ejected from ${uiModuleName} version ${uiModuleVersion}.`,
-                `*/`
+                ` * This file was ejected from ${uiModuleName} version ${uiModuleVersion}.`,
+                ` */`
             ].join("\n");
         } else {
             return [
                 `/*`,
-                `WARNING: Before modifying this file run the following command:`,
-                ``,
-                `npx keycloakify eject-file --file ${fileRelativePath.split(pathSep).join("/")}\``,
-                ``,
-                `This file comes from ${uiModuleName} version ${uiModuleVersion}.`,
-                `*/`
-            ];
+                ` *`,
+                ` * WARNING: Before modifying this file run the following command:`,
+                ` * `,
+                ` * $ npx keycloakify eject-file --file ${fileRelativePath.split(pathSep).join("/")}`,
+                ` * `,
+                ` * This file comes from ${uiModuleName} version ${uiModuleVersion}.`,
+                ` *`,
+                ` */`
+            ].join("\n");
         }
     })();
 
