99b8f1e789ff228bda68f0e858a6fa322bd2d6ed
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-03 22:57:57 +0100

    Update i18n account multi-page boilerplate

--- src/bin/initialize-account-theme/multi-page-boilerplate/i18n.ts
+++ src/bin/initialize-account-theme/multi-page-boilerplate/i18n.ts
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { i18nBuilder } from "keycloakify/account";
 import type { ThemeName } from "../kc.gen";
 
-/** @see: https://docs.keycloakify.dev/i18n */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/** @see: https://docs.keycloakify.dev/features/i18n */
 const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
 
 type I18n = typeof ofTypeI18n;
