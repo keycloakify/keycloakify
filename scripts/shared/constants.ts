0039bf661e8b5648cc35155a6c84a76e3657beec
garronej <joseph.garrone@protonmail.com>
2025-08-19 13:05:34 +0200

    Update public resources

--- scripts/shared/constants.ts
+++ scripts/shared/constants.ts
@@ -1,4 +1,4 @@
 export const KEYCLOAKIFY_LOGIN_UI_ZIP_URL =
-    "https://github.com/keycloakify/login-ui/archive/f3cfe628865984bf1308856a12b04b7e4a0752a9.zip";
+    "https://github.com/keycloakify/login-ui/archive/da875a29510ab9c8037772a0b96725f5935905d2.zip";
 export const KEYCLOAKIFY_ACCOUNT_MULTI_PAGE_UI_ZIP_URL =
     "https://github.com/keycloakify/keycloak-account-multi-page-ui/archive/75699a5b4a7ee8c09c5700d4f2fe4bb3bff330ef.zip";
