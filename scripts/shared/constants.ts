1062bfb76babb15a7c06a6cf4c54849ac99100b8
garronej <joseph.garrone@protonmail.com>
2025-12-01 20:20:05 +0100

    #968 Manually add missing translations for passkey-doAuthenticate

--- scripts/shared/constants.ts
+++ scripts/shared/constants.ts
@@ -1,4 +1,4 @@
 export const KEYCLOAKIFY_LOGIN_UI_ZIP_URL =
-    "https://github.com/keycloakify/login-ui/archive/2795e22990445337f83b5045706414c7cca187ff.zip";
+    "https://github.com/keycloakify/login-ui/archive/3f51a6fe5a00d806e609f501b5cf8697007d65f1.zip";
 export const KEYCLOAKIFY_ACCOUNT_MULTI_PAGE_UI_ZIP_URL =
     "https://github.com/keycloakify/keycloak-account-multi-page-ui/archive/75699a5b4a7ee8c09c5700d4f2fe4bb3bff330ef.zip";
